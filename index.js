f037872c73c45e0320739e5cae0b43fb3a87cdd6
mouadga <85169032+mouadga@users.noreply.github.com>
2023-01-25 17:40:16 +0100

    COMMIT initial files

---
+++ index.js
@@ -0,0 +1,42 @@
+let plushEL = document.getElementById("plush-EL")
+
+
+console.log(plushEL)
+
+
+let countH = 0
+
+
+function plusoneHOME() {
+   countH = countH + 1
+   plushEL.innerText = countH
+}
+function plustwoHOME() {
+   countH = countH + 2
+   plushEL.innerText = countH
+}
+function plusthreeHOME() {
+   countH = countH + 3
+   plushEL.innerText = countH
+}
+let plusgEL = document.getElementById("plusg-EL")
+
+
+console.log(plusgEL)
+
+
+let countG = 0
+
+
+function plusoneGUEST() {
+   countG = countG + 1
+   plusgEL.innerText = countG
+}
+function plustwoGUEST() {
+   countG = countG + 2
+   plusgEL.innerText = countG
+}
+function plusthreeGUEST() {
+   countG = countG + 3
+   plusgEL.innerText = countG
+}
